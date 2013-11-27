bf0d876438e5998983718567dd5322f3d237c949
Paul Kehrer <paul.l.kehrer@gmail.com>
2013-11-27 11:10:48 -0600

    import gpg key for barbican and enable gpg checks

--- recipes/_base.rb
+++ recipes/_base.rb
@@ -3,13 +3,13 @@
 # Recipe:: _base
 #
 # Copyright (C) 2013 Rackspace, Inc.
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #    http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -33,6 +33,12 @@ ruby_block "reload-internal-yum-cache" do
     Chef::Provider::Package::Yum::YumCache.instance.reload
   end
   action :nothing
+end
+
+#TODO(reaperhulk): switch to TLS when we drop a cert on that repo
+yum_key "RPM-GPG-KEY-barbican" do
+  url "http://yum-repo.cloudkeep.io/gpg"
+  action :add
 end
 
 #TODO(dmend): Use yum_repository resource instead of cookbook_file
