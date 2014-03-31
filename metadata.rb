fef4a9ae7c6ba075dd38d5cc164b5fbfcee438ec
Steven Gonzales <stevendgonzales@gmail.com>
2014-03-31 14:10:58 -0500

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cloudkeep@googlegroups.com'
 license          'Apache 2.0'
 description      'Installs/Configures barbican'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.10'
+version          '0.2.11'
 
 depends          'yum'
 depends          'yum-epel'
